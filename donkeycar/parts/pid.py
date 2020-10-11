76dd2d45c10823010e84d04ca4f950dfd71a2e8c
Ari Viitala <amt.viitala@gmail.com>
2020-10-11 17:18:13 +0300

    Fixed PID parameters

--- donkeycar/parts/pid.py
+++ donkeycar/parts/pid.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class PID(object):
 
-    def __init__(self, p=0.005, i=0.00, d=10):
+    def __init__(self, p=0.0025, i=0.00, d=1):
         self.p = p
         self.d = d
         self.i = i
